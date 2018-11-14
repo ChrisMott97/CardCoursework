e29e65d9c4fbe30a6287723fe6b3ba8ed03e6968
Chris <christopheramott@sky.com>
2018-11-14 11:48:47 +0000

    Initial Game constructor

--- src/com/exeter/Game.java
+++ src/com/exeter/Game.java
@@ -3,33 +3,25 @@ package com.exeter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
-    int numPlayers;
-    String packLocation;
+    List<Card> cards = new ArrayList<>();
+    List<Player> players = new ArrayList<>();
 
-    public Game(int numPlayers, String packLocation){
+    public Game(int numPlayers, File packFile){
+        try(BufferedReader br = new BufferedReader(new FileReader(packFile))){
+            String line = br.readLine();
 
-        File f = new File(packLocation);
-        if(f.exists() && !f.isDirectory()){
-            System.out.println("Found pack!");
-            try(BufferedReader br = new BufferedReader(new FileReader(f))){
-                String line = br.readLine();
-
-                while (line != null){
-
-                    line = br.readLine();
-                }
-            }catch(Exception e){
-                e.printStackTrace();
+            while (line != null){
+                cards.add(new Card(Integer.parseInt(line)));
+                line = br.readLine();
             }
-        }else{
-            System.out.println("Cannot find pack!");
+        }catch(Exception e){
+            e.printStackTrace();
         }
-        this.numPlayers = numPlayers;
-        this.packLocation = packLocation;
-    }
 
-//    private void validate()
+
+    }
 }
--- src/com/exeter/Player.java
+++ src/com/exeter/Player.java
@@ -1,4 +1,9 @@
 package com.exeter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private List<Card> cards = new ArrayList<>();
+
 }
