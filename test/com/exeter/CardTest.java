27e832e881945841ad29717a2c516b735570fdf5
Örs Barkanyi <ors.barkanyi@gmail.com>
2018-11-16 14:18:52 +0000

    Started unit testing

---
+++ test/com/exeter/CardTest.java
@@ -0,0 +1,11 @@
+package com.exeter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+    @Test
+    void name() {
+    }
+}
---
+++ test/com/exeter/PlayerTest.java
@@ -0,0 +1,71 @@
+package com.exeter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void setFromDeck() {
+    }
+
+    @Test
+    void setToDeck() {
+    }
+
+    @Test
+    void getFromDeck() {
+    }
+
+    @Test
+    void getToDeck() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getCards() {
+    }
+
+    @Test
+    void addCard() {
+    }
+
+    @Test
+    void initialWinCheck_whenWinningHand_returnsTrue() {
+        //arrange
+        Player SUT = new Player(1);
+        SUT.addCard(new Card(1));
+        SUT.addCard(new Card(1));
+        SUT.addCard(new Card(1));
+        SUT.addCard(new Card(1));
+
+        //act
+        boolean result = SUT.initialWinCheck();
+
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void initialWinCheck_whenNotWinningHand_returnsFalse() {
+        //arrange
+        Player SUT = new Player(1);
+        SUT.addCard(new Card(2));
+        SUT.addCard(new Card(2));
+        SUT.addCard(new Card(2));
+        SUT.addCard(new Card(2));
+
+        //act
+        boolean result = SUT.initialWinCheck();
+
+        //assert
+        assertFalse(result);
+    }
+}
